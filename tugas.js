a5ed17e1813df8fefc77b0f90717e16f8c9bec67
gojung12 <148044646+gojung12@users.noreply.github.com>
2023-11-23 19:35:48 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,53 @@
+let a = 12.5
+let b = 13.2
+let c = 15.4
+// math.round
+let bulatkan= math.round(a);
+console.log(bulatkan);
+let pembulatan= math.round(b);
+console.log(pembulatan)
+let pembulatkan= math.round(c);
+console.log(pembulatkan)
+
+// math.floor
+let bawah= math.floor(a)
+console.log(bawah)
+let kebawah= math.floor(b)
+console.log(kebawah)
+let tobawah= math.floor(c)
+console.log(tobawah)
+
+// math.ceil
+let atas= math.ceil(a)
+console.log(atas)
+let keatas= math.ceil(b)
+console.log(keatas)
+let toatas= math.ceil(c)
+console.log(toatas)
+
+
+// math.pow
+let d= 1
+let e= 5
+let f=7
+let g=10
+let h=12
+
+let pangkat= math.pow(5,7)
+console.log(pangkat)
+let kepangkat= math.pow(10,7)
+console.log(kepangkat)
+let topangkat= math.pow(1,7)
+console.log(topangkat)
+
+
+let i= 10
+let j= 20
+let k= 15
+
+let akar= math.trunc(i)
+console.log(akar)
+let keakar= math.trunc(j)
+console.log(keakar)
+let toakar= math.trunc(k)
+console.log(toakar)
